83d37cd63131b0cf50a45e4c467ca2fd0cc08dc8
Danil Volkov <volchara287@gmail.com>
2024-01-23 04:50:54 +0400

    feat: выносит в отдельный файл команды бота

---
+++ src/botCommands.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    command: 'registration',
+    description: 'Регистрация нового пользователя',
+  },
+  {
+    command: 'stats',
+    description: 'Статистика по проделанным работам',
+  },
+  {
+    command: 'balance',
+    description: 'Информация о балансе',
+  },
+];
