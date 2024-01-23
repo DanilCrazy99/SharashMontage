8a10dce8ede42c08707785fa9daab6c8f1567290
Danil Volkov <volchara287@gmail.com>
2024-01-23 04:50:08 +0400

    feat: выносит функции для клавиатур в отдельный файл
    Создаёт функции для создания параметров inline_keyboard и также dictionary  для генерации inline клавиатур по умолчанию

--- src/keyboards.js
+++ src/keyboards.js
@@ -1,26 +1,54 @@
-export default {
+/* eslint-disable camelcase */
+const dictionary = {
   registration: [
-    [
-      {
-        id: 'name',
-        text: 'Имя и Фамилия',
-        callback_data: 'registration_name',
-      },
-    ],
-    [
-      {
-        id: 'phone',
-        text: 'Номер телефона',
-        callback_data: 'registration_phone',
-      },
-    ],
-    [
-      {
-        id: 'card',
-        text: 'Номер карты(для перевода зарплаты)',
-        callback_data: 'registration_card',
-      },
-    ],
+    {
+      text: 'Имя и Фамилия',
+      callback_data: 'registration_name',
+    },
+    {
+      text: 'Номер телефона',
+      callback_data: 'registration_phone',
+    },
+    {
+      text: 'Номер карты(для перевода зарплаты)',
+      callback_data: 'registration_card',
+    },
   ],
-  // keyboard: [[{ text: 'hello' }, { text: 'hi' }]],
 };
+// keyboard: [[{ text: 'hello' }, { text: 'hi' }]],
+// [{text, callback_data}, {}, ...]
+// newData = { text, callback_data }
+const makeColumnMarkup = (arrData, newData) => {
+  if (newData) {
+    const { callback_data: newDataCallback = null, text: newDataText } = newData;
+    return arrData.map(({ text, callback_data }) => (newDataCallback === callback_data
+      ? [{ text: newDataText, callback_data }]
+      : [{ text, callback_data }]));
+  }
+  return arrData
+    .map(({ text, callback_data }) => [{ text, callback_data }]);
+};
+
+const makeEffect = async (bot, appState, paramName, updateStateFunc, { chat: { id }, text }) => {
+  const { keyboard: { addMsgId }, registration } = appState;
+  bot.deleteMessage(id, addMsgId);
+  const { [paramName]: prevValue } = registration[id];
+  const newState = updateStateFunc('process', text);
+  // appState.registration = update(appState.registration, {
+  //   [id]: { $merge: { status: 'process', name: text, addMsg: null } },
+  // });
+  if (newState.registration[id][paramName] !== prevValue) {
+    const { reply_markup, message_id } = await bot.editMessageReplyMarkup({
+      inline_keyboard: makeColumnMarkup(
+        dictionary.registration,
+        { text, callback_data: `registration_${paramName}` },
+      ),
+    }, { chat_id: id, message_id: newState.registration[id].mainMsgId });
+    updateStateFunc(reply_markup, message_id);
+    // appState.keyboard = update(appState.keyboard, {
+    //   [id]: { $set: { reply_markup, message_id } },
+    // });
+  }
+};
+
+export { dictionary, makeColumnMarkup, makeEffect };
